b3213eeb6c90171b3d35bbebad4a669e1a324690
RuslanTsitser <cicer_ruslan@mail.ru>
2023-05-06 10:19:02 +0300

    first commit

---
+++ example/telegram_web_app_client_example.dart
@@ -0,0 +1,6 @@
+import 'package:telegram_web_app_client/telegram_web_app_client.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/telegram_web_app_client.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library telegram_web_app_client;
+
+export 'src/telegram_web_app_client_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
